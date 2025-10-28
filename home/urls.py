5224e3684cb10f266caff94a5c9292ddfa96a0f0
Kuwushal <kuwushal1928@gmail.com>
2025-10-28 19:10:46 +0545

    Initial commit - Django Jetflix project with registration & login

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.register_view, name='register'), 
+     path('login/', views.login_view, name='login'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-def home(request):
-    return HttpResponse("helllo world")
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created. You can now log in.")
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'home/register.html', {'form': form})
 
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'home/login.html', {'form': form})
